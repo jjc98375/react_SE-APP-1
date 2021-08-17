0caf25d4c3a13d4db485c9d9087c2f99be8c8fe4
Joshua Cho <collegecho98@gmail.com>
2021-08-17 16:56:47 +0900

    hehe added

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h2>Welcome to our discussion forum</h2>
         <i>All copyrights are reserved by Jaehun and his manager.</i>
+        <p>hehe</p>
       </header>
     </div>
   );
